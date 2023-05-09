9425d007a0ed35981980a5897f70d91a7e19d76c
tema2 <play.aab.25122001@gmail.com>
2023-05-09 21:49:11 +0200

    make tabs to work

--- program/web-ddlog/src/components/project_area.rs
+++ program/web-ddlog/src/components/project_area.rs
@@ -27,17 +27,41 @@ pub fn project_area(props: &Props) -> Html {
         }
     });
 
-    let new_cb = dispatch.reduce_mut_callback(|state| {
-        state.add_empty_module("new")
+    let new_name = use_state(|| -> Option<String>{ None });
+    let new_name2 = new_name.clone();
+    let new_name3 = new_name.clone();
+    let new_name4 = new_name.clone();
+    let new_name5 = new_name.clone();
+
+    let new_cb = dispatch.reduce_mut_callback(move |state| {
+        if let Some(name) = &*new_name4 {
+            state.add_empty_module(&name);
+            new_name5.set(None)
+        }
     });
 
-    
+     
     html! {
         <div>
             <ul class="nav">
                 {for tabs}
                 <li class="nav-item" >
-                    <a class="nav-link active" onclick={&new_cb}>{"+"}</a>
+                    if new_name.is_none() {
+                        <a class="nav-link active" onclick={move |_| new_name3.set(Some(String::new()))}>{"+"}</a>
+                    }
+                    if let Some(name) = &*new_name {
+                        <div>
+                            <input type="text" value={name.clone()} onchange={
+                                move |ev: Event| {ev.target().and_then(
+                                    |trg| trg.dyn_into::<HtmlInputElement>().ok()
+                                ).map(
+                                    |input| new_name2.set(Some(input.value()))
+                                );}
+                            }/>
+                            <button onclick={&new_cb}>{"+"}</button>
+                            <button onclick={move |_| new_name.set(None)}>{"-"}</button>
+                        </div>
+                    }
                 </li>
             </ul>
             if let Some(tab) = state.get_current_module() { <Editor name={tab} /> }
